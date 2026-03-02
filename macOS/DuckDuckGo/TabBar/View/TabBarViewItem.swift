13003c17d9d52f3ce167484f8e082f1f51bb7211
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2026-03-02 15:17:22 -0300

    Guard pinned tabs from being suspended

--- macOS/DuckDuckGo/TabBar/View/TabBarViewItem.swift
+++ macOS/DuckDuckGo/TabBar/View/TabBarViewItem.swift
@@ -1415,6 +1415,7 @@ extension TabBarViewItem: NSMenuDelegate {
     }
 
     private func addSuspendResumeMenuItem(to menu: NSMenu) {
+        guard !isPinned else { return }
         let isSuspended = tabViewModel?.isSuspended ?? false
         let title = isSuspended ? UserText.resumeTab : UserText.suspendTab
         let menuItem = NSMenuItem(title: title, action: #selector(suspendTabAction(_:)), keyEquivalent: "")
