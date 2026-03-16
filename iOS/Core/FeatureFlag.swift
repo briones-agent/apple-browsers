a0d9dd795f0a62fab1554b58c95f1c5e7d8c14e4
Alessandro Boron <aboron@duckduckgo.com>
2026-03-16 17:14:34 +1100

    Force Onboarding Rebranding

--- iOS/Core/FeatureFlag.swift
+++ iOS/Core/FeatureFlag.swift
@@ -751,7 +751,7 @@ extension FeatureFlag: FeatureFlagDescribing {
         case .crashCollectionLimitCallStackTreeDepth:
             return .remoteReleasable(.subfeature(iOSBrowserConfigSubfeature.crashCollectionLimitCallStackTreeDepth))
         case .onboardingRebranding:
-            return .disabled
+            return .remoteReleasable(.feature(.iOSBrowserConfig))
         case .webExtensions:
             return .remoteReleasable(.feature(.webExtensions))
         case .embeddedExtension:
