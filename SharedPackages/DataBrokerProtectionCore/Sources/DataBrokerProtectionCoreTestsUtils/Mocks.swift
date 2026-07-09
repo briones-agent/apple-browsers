fbbb1067bad59cf5fdecd9d5c82218fcf506ea03
Anh Do <ado@duckduckgo.com>
2026-07-09 12:46:50 -0400

    Fix PIR RMF prerequisite wiring

--- SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCoreTestsUtils/Mocks.swift
+++ SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCoreTestsUtils/Mocks.swift
@@ -1113,8 +1113,6 @@ public final class MockDatabase: DataBrokerProtectionRepository {
     public var updatePreferredRunDateError: Error?
     public var updateSubmittedSuccessfullyDateError: Error?
     public var brokerProfileQueryDataError: Error?
-    public var hasMatchesToReturn = false
-    public var hasMatchesError: Error?
 
     public lazy var callsList: [Bool] = [
         wasSaveProfileCalled,
