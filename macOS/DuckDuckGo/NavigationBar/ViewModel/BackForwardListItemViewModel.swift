9c236433230615d7d8139abc6a9061d1f00ad31e
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-07-21 16:38:39 -0300

    Addressing AI feedback

--- macOS/DuckDuckGo/NavigationBar/ViewModel/BackForwardListItemViewModel.swift
+++ macOS/DuckDuckGo/NavigationBar/ViewModel/BackForwardListItemViewModel.swift
@@ -82,7 +82,7 @@ final class BackForwardListItemViewModel {
         case .bookmarks:
             return DesignSystemRebrand.isAppRebranded() ? DesignSystemImages.Color.Size16.bookmarksNew : .bookmarksFolder
         case .history:
-            return DesignSystemRebrand.isAppRebranded() ? DesignSystemImages.Color.Size16.history : .bookmarksFolder
+            return DesignSystemRebrand.isAppRebranded() ? DesignSystemImages.Color.Size16.history : .historyFavicon
         case let url where url?.isDuckPlayer == true:
             return .duckPlayer
         default:
