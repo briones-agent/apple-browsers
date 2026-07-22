0d9518b38c51108924a31133c3742cc460332036
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-07-22 10:16:28 -0300

    Updates Shield Animation

--- macOS/DuckDuckGo/VisualRefresh/PrivacyShieldAddressBarStyleProviding.swift
+++ macOS/DuckDuckGo/VisualRefresh/PrivacyShieldAddressBarStyleProviding.swift
@@ -46,7 +46,7 @@ final class LatestPrivacyShieldAddressBarStyleProvider: PrivacyShieldAddressBarS
     let iconWithDot: NSImage = DesignSystemImages.Color.Size16.shieldNeutralAlert
 
     let hoverAnimation: String = "Shield-Color-24-Hover"
-    let hoverAnimationWithDot: String = "Shield-Outline-24-Light"
+    let hoverAnimationWithDot: String = "Shield-Off-Gray"
     let animationForShield: String = "Shield-Color-24"
-    let animationForShieldWithDot: String = "Shield-Outline-24-Light"
+    let animationForShieldWithDot: String = "Shield-Off-Gray"
 }
